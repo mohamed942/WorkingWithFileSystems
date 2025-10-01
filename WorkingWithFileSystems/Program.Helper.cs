87b7bedd481379b9a6d8b1bb4f5165f5364eb23c
Mohamed Emad <memad0699@gmail.com>
2025-10-02 02:58:38 +0300

    Add project files.

---
+++ WorkingWithFileSystems/Program.Helper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.IO.Directory;
+using static System.Console;
+using static System.IO.Path;
+using static System.Environment;
+
+namespace WorkingWithFileSystems
+{
+    public partial class Program
+    {
+        static void SectionTitle(string title)
+        {
+            ConsoleColor previousColor = ForegroundColor;
+            ForegroundColor = ConsoleColor.Yellow;
+            WriteLine("*");
+            WriteLine($"* {title}");
+            WriteLine("*");
+            ForegroundColor = previousColor;
+        }
+    }
+}
